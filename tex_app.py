f6e91e0dceefe2b51bc98d6aeb5bea2a33b3a765
Caio De Naday Hornhart <caiodnh@protonmail.com>
2024-01-04 18:36:39 -0330

    Add an example of automatic latex creation

---
+++ tex_app.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+# LaTeX document content
+latex_content = r'''
+\documentclass{article}
+\begin{document}
+Hello, world!
+\end{document}
+'''
+
+# Create a directory for the LaTeX project if it doesn't exist
+directory = "latex_project"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+# Path to the LaTeX file
+file_path = os.path.join(directory, "document.tex")
+
+# Write the LaTeX content to the file
+with open(file_path, 'w') as file:
+    file.write(latex_content)
+
+# Compile the LaTeX file into a PDF
+# This requires pdflatex to be installed on your system
+compile_command = f"pdflatex -output-directory={directory} {file_path}"
+subprocess.run(compile_command, shell=True, check=True)
+
+print(f"LaTeX document compiled: {os.path.join(directory, 'document.pdf')}")
+

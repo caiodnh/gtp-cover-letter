c29b64658eeba5725c22a13085b4dacc4bd028b2
Caio De Naday Hornhart <caiodnh@protonmail.com>
2024-01-11 12:16:18 -0330

    Add more forms and improve design of webpage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template, render_template_string
 
 app = Flask(__name__)
 
@@ -28,7 +28,7 @@ def form():
         summary = request.form['summary']
         print(f"Name: {name}, Summary: {summary}")  # You can process the data as needed
         return f"Received: Name - {name}, Summary - {summary}"
-    return render_template_string(html)
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

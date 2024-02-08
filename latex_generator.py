09347a3d9d68314462a00d5a2cbaa5e39b7c27d2
Caio De Naday Hornhart <caiodnh@protonmail.com>
2024-02-08 10:13:13 -0330

    Fix latex generation to comply with new .data from CoverLetter class

--- latex_generator.py
+++ latex_generator.py
@@ -19,18 +19,18 @@ class LatexMixin:
 
             return camel_case
 
-        return f"cover_letters/{date}_{to_pascal_case(self.company_name)}_{to_pascal_case(self.job_title)}"
+        # Accessing company name and job title from self.data dictionary
+        return f"cover_letters/{date}_{to_pascal_case(self.data['company_name'])}_{to_pascal_case(self.data['job_title'])}"
 
     @property
     def latex_macros_file(self):
         file = (
-            # geometry will play a bigger role in future versions
             r"\newcommand{\geometryConditions}{\geometry{margin=1in}}" + "\n" +
-            r"\newcommand{\candidateName}{" +self.candidate_name + r"}" + "\n" +
+            r"\newcommand{\candidateName}{" + self.data['candidate_name'] + r"}" + "\n" +
             # Note that we edit the address to have "\\" instead of "\n"
-            r"\newcommand{\candidateAddress}{" + self.candidate_address.replace("\n", r"\\") + r"}" + "\n" +
-            r"\newcommand{\companyAddress}{" + self.company_address + r"}" + "\n" +
-            r"\newcommand{\hiringManager}{" + self.hiring_manager + r"}" + "\n"
+            r"\newcommand{\candidateAddress}{" + self.data['candidate_address'].replace("\n", r"\\") + r"}" + "\n" +
+            r"\newcommand{\companyAddress}{" + self.data['company_address'] + r"}" + "\n" +
+            r"\newcommand{\hiringManager}{" + self.data['hiring_manager'] + r"}" + "\n"
         )
 
         return file
@@ -40,7 +40,7 @@ class LatexMixin:
         if not os.path.exists(self.latex_directory):
             os.makedirs(self.latex_directory)
 
-        # Create main file, whose content never changes (see attribute latex_main_file below)
+        # Create main file, whose content never changes
         main_file = os.path.join(self.latex_directory, "CoverLetter.tex")
         with open(main_file, 'w') as file:
             file.write(self.latex_main_file)
@@ -50,18 +50,17 @@ class LatexMixin:
         with open(macros_file, 'w') as file:
             file.write(self.latex_macros_file)
 
-        # Create body of the letter file, assumed withput xml tags
-        if self.gpt_cover_letter is None:
+        # Create body of the letter file, assuming without XML tags
+        if self.data['body'] is None:
             self.create_cover_letter()
 
         body_file = os.path.join(self.latex_directory, "body.tex")
         with open(body_file, 'w') as file:
-            file.write(self.gpt_cover_letter)
+            file.write(self.data['body'])
 
         # Compile the LaTeX file into a PDF
-        # This requires pdflatex to be installed on your system
         result = subprocess.run(
-            ['pdflatex',  '-interaction=nonstopmode', "CoverLetter.tex"],
+            ['pdflatex', '-interaction=nonstopmode', "CoverLetter.tex"],
             capture_output=True,
             text=True,
             cwd=self.latex_directory  # Set the working directory to where your LaTeX files are
@@ -69,43 +68,10 @@ class LatexMixin:
 
         # Check if pdflatex succeeded
         if result.returncode == 0:
-            print(f"LaTeX compilation successful.")
+            print("LaTeX compilation successful.")
         else:
             print("LaTeX compilation failed.")
             print("stdout:", result.stdout)
             print("stderr:", result.stderr)
 
         return result.returncode
-
-    # main file, it never changes
-    latex_main_file = r'''
-\documentclass[11pt]{letter}
-\usepackage[utf8]{inputenc}
-\usepackage[T1]{fontenc}
-\usepackage{geometry}
-
-% Input file with macros:
-% \geometry_conditions, \candidate_name, \candidate_address, \company_address, \hiring_manager
-\input{macros}
-
-% Macro using ``geometry`` package
-\geometryConditions
-
-\address{\candidateName\\ \candidateAddress}
-\date{}
-\signature{\candidateName}
-
-\begin{document}
-
-\begin{letter}{\companyAddress}
-
-\opening{Dear \hiringManager,}
-
-% Input letter's main body
-\input{body}
-
-\closing{Sincerely,}
-
-\end{letter}
-\end{document}
-'''
